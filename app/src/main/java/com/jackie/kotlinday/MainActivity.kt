90a9188a9a5f567148684e18e48296ba41b304c7
jackie <zhangjie.456@163.com>
2019-04-11 17:29:16 +0800

    test

--- app/src/main/java/com/jackie/kotlinday/MainActivity.kt
+++ app/src/main/java/com/jackie/kotlinday/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    findViewById<TextView>(R.id.tv).text="hello kotlin"
+    findViewById<TextView>(R.id.tv).text="hello world"
     }
 
 }
